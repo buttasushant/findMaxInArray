572a0a060499bdce9676d1f8fc7241a54b6f20c6
Sushant Butta <sushant.butta@gmail.com>
2024-05-31 22:39:39 +0530

    updated to use Integer.MIN_VALUE

--- src/main/java/org/buttas/Main.java
+++ src/main/java/org/buttas/Main.java
@@ -2,6 +2,14 @@ package org.buttas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //find max in an array
+        int [] arr = {11,212,34,4,5};
+        int max = Integer.MIN_VALUE ;
+        for (int n : arr){
+            if(n > max){
+                max = n;
+            }
+        }
+        System.out.println(max);
     }
 }
